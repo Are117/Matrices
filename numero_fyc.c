11e8a13330b30d6da388de1f2ed70367b415775c
Are117 <arelysajilaaa@gmail.com>
2024-07-08 00:50:19 -0500

    Update numero_fyc.c

--- numero_fyc.c
+++ numero_fyc.c
@@ -8,11 +8,9 @@ int matriz[3][3]={{1,2,3}, //definición de matriz
                   {5,6,7},
                   {9, 10, 11}};
 
-
-
-int elementos=sizeof(matriz);//le asigna a la variable elementos el tamaño de la matriz
-int filas=elementos/12;//como el tamaño de la matriz es 36 y cada elemento vale 4 entonces dividimos para 12 para obtener 3
-int columnas=elementos/12;///hacemos lo mismo con columnas
+int tamaño=sizeof(matriz)/sizeof(matriz[0]);
+int filas=tamaño;
+int columnas=tamaño;
 
 printf("La matriz tiene una dimension de %dx%d\n", filas, columnas);
 return 0;
